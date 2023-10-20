923a21bfcc30f458c7057a8c0da278fa65c24cbf
Muraylo, Yuliy <yuliy.muraylo@stud.th-luebeck.de>
2023-10-21 01:09:06 +0200

    updated bunny smack and jump

---
+++ enemies/bungaroo.gd
@@ -0,0 +1,35 @@
+extends "res://enemies/enemy_base.gd"
+
+
+func enter_walk():
+	super.enter_walk()
+
+
+func walk_state():
+	super.walk_state()
+	if velocity.y > 0:
+		skin.frame = 0
+	else:
+		skin.frame = 1
+	if is_on_floor():
+		velocity.y = -160
+
+
+func enter_chase():
+	super.enter_chase()
+
+
+func chase_state():
+	super.chase_state()
+	if not is_attacking:
+		if velocity.y > 0:
+			skin.frame = 0
+		else:
+			skin.frame = 1
+			
+		if is_on_floor():
+			velocity.y = -160
+
+
+func attack():
+	super.attack()
